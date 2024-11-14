2a60e4b5a0417c8da2c52fdfef92ff59beeea25f
alexml47 <amoldovan485@alumnes.politecnicllevant.cat>
2024-11-14 21:02:03 +0100

    as

--- nasa/src/main/java/org/example/nasa/dao/AproachDao.java
+++
@@ -1,6 +0,0 @@
-package org.example.nasa.dao;
-
-import org.example.nasa.model.Aproach;
-
-public interface AproachDao extends GeneralDao<Aproach>{
-}
--- nasa/src/main/java/org/example/nasa/dao/GeneralDao.java
+++
@@ -1,9 +0,0 @@
-package org.example.nasa.dao;
-
-import java.util.List;
-
-public interface GeneralDao <T>{
-    List<T> findAll();
-    T findById(int id);
-    void save(T obj);
-}
