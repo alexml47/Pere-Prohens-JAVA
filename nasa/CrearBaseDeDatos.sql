9d9055f87d14899646295e8ef1c6a55b6da41cc3
alexml47 <amoldovan485@alumnes.politecnicllevant.cat>
2024-11-24 22:26:14 +0100

    base de datos

--- nasa/CrearBaseDeDatos.sql
+++ nasa/CrearBaseDeDatos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: Nov 24, 2024 at 04:56 PM
+-- Generation Time: Nov 24, 2024 at 09:25 PM
 -- Server version: 8.0.40
 -- PHP Version: 8.2.24
 
@@ -28,32 +28,38 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `aproach` (
-  `id` int NOT NULL,
-  `aproachDate` date NOT NULL,
-  `velocity` double NOT NULL,
-  `distance` double NOT NULL,
-  `orbitingBody` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `id_asteroid` int NOT NULL
+                           `id` int NOT NULL,
+                           `aproachDate` date NOT NULL,
+                           `velocity` double NOT NULL,
+                           `distance` double NOT NULL,
+                           `orbitingBody` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+                           `id_asteroid` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `asteroid`
 --
 
 CREATE TABLE `asteroid` (
-  `id` int NOT NULL,
-  `name` varchar(100) DEFAULT NULL,
-  `magnitude` double DEFAULT NULL,
-  `diameter` double DEFAULT NULL,
-  `dangerous` varchar(100) DEFAULT NULL,
-  `nasaAsteroid` tinyint(1) NOT NULL
+                            `id` int NOT NULL,
+                            `name` varchar(100) DEFAULT NULL,
+                            `magnitude` double DEFAULT NULL,
+                            `diameter` double DEFAULT NULL,
+                            `dangerous` varchar(100) DEFAULT NULL,
+                            `nasaAsteroid` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `rol`
+--
 
 CREATE TABLE `rol` (
-  `id` int NOT NULL,
-  `rol` varchar(100) NOT NULL
+                       `id` int NOT NULL,
+                       `rol` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
@@ -61,8 +67,8 @@ CREATE TABLE `rol` (
 --
 
 INSERT INTO `rol` (`id`, `rol`) VALUES
-(1, 'astronomer'),
-(2, 'observer');
+                                    (1, 'astronomer'),
+                                    (2, 'observer');
 
 -- --------------------------------------------------------
 
@@ -71,36 +77,40 @@ INSERT INTO `rol` (`id`, `rol`) VALUES
 --
 
 CREATE TABLE `user` (
-  `id` int NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `id_rol` int NOT NULL
+                        `id` int NOT NULL,
+                        `name` varchar(100) NOT NULL,
+                        `password` varchar(100) NOT NULL,
+                        `id_rol` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Indexes for dumped tables
+--
 
+--
 -- Indexes for table `aproach`
-
+--
 ALTER TABLE `aproach`
-  ADD PRIMARY KEY (`id`),
+    ADD PRIMARY KEY (`id`),
   ADD KEY `id_asteroid` (`id_asteroid`);
 
 --
 -- Indexes for table `asteroid`
 --
 ALTER TABLE `asteroid`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `rol`
 --
 ALTER TABLE `rol`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`),
+    ADD PRIMARY KEY (`id`),
   ADD KEY `id_rol` (`id_rol`);
 
 --
@@ -111,25 +121,25 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `aproach`
 --
 ALTER TABLE `aproach`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1423;
+    MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `asteroid`
 --
 ALTER TABLE `asteroid`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+    MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=492;
 
 --
 -- AUTO_INCREMENT for table `rol`
 --
 ALTER TABLE `rol`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+    MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+    MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Constraints for dumped tables
@@ -139,13 +149,13 @@ ALTER TABLE `user`
 -- Constraints for table `aproach`
 --
 ALTER TABLE `aproach`
-  ADD CONSTRAINT `aproach_ibfk_1` FOREIGN KEY (`id_asteroid`) REFERENCES `asteroid` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+    ADD CONSTRAINT `aproach_ibfk_1` FOREIGN KEY (`id_asteroid`) REFERENCES `asteroid` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
 -- Constraints for table `user`
 --
 ALTER TABLE `user`
-  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+    ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
